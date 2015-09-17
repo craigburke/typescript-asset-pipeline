f1eaab30c78ef7a88cd91259bc9eb40407860990
Craig Burke <craig@craigburke.com>
2015-09-17 08:19:41 -0400

    Reworking getReferenceFiles method

--- src/main/groovy/com/craigburke/asset/TypeScriptProcessor.groovy
+++ src/main/groovy/com/craigburke/asset/TypeScriptProcessor.groovy
@@ -36,22 +36,22 @@ class TypeScriptProcessor extends JavaScriptProcessor {
         value.replace("'", "\\'").replaceAll("(\r)?\n", " \\\\n")
     }
 
-    static Map getReferenceFiles(AssetFile assetFile, String basePath = '') {
+    static Map getReferenceFiles(AssetFile assetFile, AssetFile baseFile = null) {
         Map references = [:]
+        baseFile = baseFile ?: assetFile
 
         assetFile.inputStream.text.findAll(~/reference path=\s*"(.+?)"/) { String fullMatch, String value ->
-            String path = AssetHelper.normalizePath("${assetFile.parentPath ?: ''}/${value}") - basePath
+            String path = AssetHelper.normalizePath("${assetFile.parentPath ?: ''}/${value}") - baseFile.path
             path = path.startsWith('/') ? path.substring(1) : path
 
             AssetFile referenceFile = AssetHelper.fileForFullName(path)
             if (referenceFile) {
                 references[path] = formatJavascriptAsString(referenceFile.inputStream.text)
-                references << getReferenceFiles(referenceFile, basePath ?: assetFile.path)
+                references << getReferenceFiles(referenceFile, baseFile)
             }
         }
 
         references
     }
-
 
 }
