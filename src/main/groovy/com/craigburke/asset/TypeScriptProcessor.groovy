0038f42908a65b31e72a2db3eef8cd08e1e56578
Craig Burke <craig@craigburke.com>
2015-09-17 05:20:59 -0400

    Loading d.ts reference files

--- src/main/groovy/com/craigburke/asset/TypeScriptProcessor.groovy
+++ src/main/groovy/com/craigburke/asset/TypeScriptProcessor.groovy
@@ -2,6 +2,7 @@ package com.craigburke.asset
 
 import asset.pipeline.AssetCompiler
 import asset.pipeline.AssetFile
+import asset.pipeline.AssetHelper
 import groovy.transform.Synchronized
 
 class TypeScriptProcessor extends JavaScriptProcessor {
@@ -18,11 +19,36 @@ class TypeScriptProcessor extends JavaScriptProcessor {
     }
 
     String process(String input, AssetFile assetFile) {
-        String formattedInput = input.replace("'", "\\'").replaceAll("(\r)?\n", " \\\\n")
+        if (assetFile.name.endsWith('d.ts')) {
+            return input
+        }
 
         javaScript {
-            eval "ts.compileString('${formattedInput}');"
-        }
+            getReferenceFiles(assetFile).each { String name, String data ->
+                eval "ts.addReferenceFile('${name}', '${data}');"
+            }
+            eval "ts.compile('${assetFile.name}', '${formatJavascriptAsString(input)}');"
+        }.replace('\\n', '\n')
     }
+
+    static String formatJavascriptAsString(String value) {
+        value.replace("'", "\\'").replaceAll("(\r)?\n", " \\\\n")
+    }
+
+    static Map getReferenceFiles(AssetFile assetFile, String basePath = '') {
+        Map references = [:]
+
+        assetFile.inputStream.text.findAll(~/reference path=\s*"(.+?)"/) { String fullMatch, String value ->
+            String path = AssetHelper.normalizePath("${assetFile.parentPath ?: ''}/${value}") - basePath
+            AssetFile referenceFile = AssetHelper.fileForFullName(path)
+            if (referenceFile) {
+                references[path] = formatJavascriptAsString(referenceFile.inputStream.text)
+                references << getReferenceFiles(referenceFile, basePath ?: assetFile.path)
+            }
+        }
+
+        references
+    }
+
 
 }
